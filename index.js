752743dac356aa510041c82aaa474cfb7cbd9c05
jesse keane <me@jessekeane.me>
2013-06-19 06:25:23 +0400

    adding more tests

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function template(thing) {
   var str = thing.toString();
   return function (obj) {
     return str.replace(/\{\{\s*(\w+)\s*\}\}/g, function (chunk, word) {
-      return obj[word] ? obj[word] : chunk;
+      return obj[word];
     });
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
 var template = require('../index.js'),
     assert = require('assert'),
     view = 'hello {{ world }} how is your {{ weekday }} going?',
-    data = {
-             world: 'dude',
-             weekday: 'tuesday'
-           },
+    data = { world: 'dude', weekday: 'tuesday' },
     tpl = template(view);
 
+// check it matches initial conditions
 assert.equal(tpl(data), 'hello dude how is your tuesday going?');
+
+// make sure it works for varied values
+data = { world: 'lady', weekday: 'thursday' };
+
+assert.equal(tpl(data), 'hello lady how is your thursday going?');
+
+// return undefined for undefined values
+data = { world: 'friendo' };
+
+assert.equal(tpl(data), 'hello friendo how is your undefined going?');
