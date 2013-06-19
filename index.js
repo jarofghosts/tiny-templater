a29e4c14cb5b93be5e6658faee5095009e940bcc
jesse keane <me@jessekeane.me>
2013-06-19 08:41:28 +0400

    added tests, updated regex to allow for dots

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var dotpather = require('dotpather');
+
 function template(thing) {
   var str = thing.toString();
   return function (obj) {
-    return str.replace(/\{\{\s*(\w+)\s*\}\}/g, function (chunk, word) {
-      return obj[word];
+    return str.replace(/\{\{\s*([a-zA-Z0-9_\.]+)\s*\}\}/g, function (chunk, word) {
+      return dotpather(word)(obj);
     });
   }
 }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,12 @@ var template = require('../index.js'),
     assert = require('assert'),
     view = 'hello {{ world }} how is your {{ weekday }} going?',
     data = { world: 'dude', weekday: 'tuesday' },
-    tpl = template(view);
+    view2 = '{{ person.name }} how is your {{ week.day }}?',
+    view3 = '{{ greeting }}, {{ person.name }} or "{{ person.nicknames.favorite }}": how is your {{ week.day }}?',
+    data2 = { greeting: 'hey', person: { name: 'Robert', nicknames: { favorite: 'Robbie' } }, week: { day: 'Friday' } },
+    tpl = template(view),
+    tpl2 = template(view2),
+    tpl3 = template(view3);
 
 // check it matches initial conditions
 assert.equal(tpl(data), 'hello dude how is your tuesday going?');
@@ -16,3 +21,9 @@ assert.equal(tpl(data), 'hello lady how is your thursday going?');
 data = { world: 'friendo' };
 
 assert.equal(tpl(data), 'hello friendo how is your undefined going?');
+
+// check deep object notation
+assert.equal(tpl2(data2), 'Robert how is your Friday?');
+
+// verify differing levels work
+assert.equal(tpl3(data2), 'hey, Robert or "Robbie": how is your Friday?');
