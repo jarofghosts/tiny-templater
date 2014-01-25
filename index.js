7e29d1794330f607df23b9f7df05bb1ec25b146a
jesse keane <me@jessekeane.me>
2014-01-25 01:45:56 -0800

    minor update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = template
 
-var dotpather = require('dotpather')
+var dotpather = require('dotpather'),
+    template_rex = /\{\{\s*([\w_\.]+)\s*\}\}/g
 
 function template(thing) {
   var str = thing.toString()
   return function template_generator(obj) {
-    return str.replace(/\{\{\s*([\w_\.]+)\s*\}\}/g, template_lookup)
+    return str.replace(template_rex, template_lookup)
 
     function template_lookup(chunk, word) {
       return dotpather(word)(obj)
